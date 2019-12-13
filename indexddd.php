531d26af7f2a35343a0812dd1b749b872bf11bf1
Razoxk <Hood.e019!>
2019-12-12 23:39:06 -0300

    fgg

--- indexddd.php
+++ indexddd.php
@@ -174,7 +174,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="title">
-						<h2>¿A quiénes está dirigido?</h2>
+						<h2>Programas</h2>
 					</div>
 					<div class="col-md-4 col-sm-6 col-xs-6">
 						<div class="block wow fadeInRight" data-wow-delay=".3s">
